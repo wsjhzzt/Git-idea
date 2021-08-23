140077acbc0eab2eb5bc14a242cb97994b507117
zzt <2337810704@qq.com>
2021-08-23 10:34:05 +0800

    master commit2

--- src/main/java/com/zzt/git/GitTest.java
+++ src/main/java/com/zzt/git/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hell git");
         System.out.println("hell git");
-        System.out.println("hell git213");
+        System.out.println("主人的命令罢了");
         System.out.println("你好,热修");
     }
 }
