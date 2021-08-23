410936d0e9640d940912f2e5d68b0fb96d1169f1
zzt <2337810704@qq.com>
2021-08-23 10:25:46 +0800

    hot-fix commit2

--- src/main/java/com/zzt/git/GitTest.java
+++ src/main/java/com/zzt/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hell git");
         System.out.println("hell git213");
         System.out.println("你好,热修");
+        System.out.println("再次你好,热修");
     }
 }
